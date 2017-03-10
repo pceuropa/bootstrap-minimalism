dd3cf24b350b9c91b0a1592a27931619b7dd23ae
Rafal Marguzewicz <info@pceuropa.net>
2017-03-10 22:16:01 +0000

    face

--- sidebar.php
+++ sidebar.php
@@ -3,3 +3,4 @@ if ( is_active_sidebar( 'sidebar-1' ) ){
 	dynamic_sidebar( 'sidebar-1' ); 
 }; 
  ?>
+<span class="fb-like" data-href="https://www.facebook.com/PCEUROPA/" data-layout="standard" data-action="like" data-size="small" data-show-faces="false" data-share="true"></span>
